8e138c843fd7b16dc1b085d02adf83c02cb7b59d
Vincze Roland <vrolandd.public@gmail.com>
2025-11-03 23:51:45 +0100

    Netlify fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import { reactRouter } from "@react-router/dev/vite";
+
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [tailwindcss(), netlifyReactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), netlifyReactRouter(), tsconfigPaths()],
 });
